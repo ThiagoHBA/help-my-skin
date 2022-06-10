5583a5d6cf8307748b795fd3194beb805ab9b5d0
thiagoHBA <thiagohhab@gmail.com>
2022-06-10 10:17:10 -0300

    adding more component references in view controller and testing then

--- HelpMySkin/Sources/View/TodaysRoutine/TodaysRoutineViewController.swift
+++ HelpMySkin/Sources/View/TodaysRoutine/TodaysRoutineViewController.swift
@@ -10,17 +10,40 @@ import UIKit
 class TodaysRoutineViewController: UIViewController {
 
     @IBOutlet weak var weatherCardImage: UIImageView!
+    @IBOutlet weak var lastTimeRequestText: UILabel!
+    @IBOutlet weak var temperatureText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        weatherCardImage.image = changeWeatherIcon(temperature: 10)
+        weatherCardImage.image = obtainWeatherIcon(temperature: 10)
+        lastTimeRequestText.text = obtainLastTimeRequestText(newTime: 61)
+        temperatureText.text = obtainTemperatureText(newValue: -30)
     }
-    func changeWeatherIcon (temperature: Double) -> UIImage {
+    func obtainWeatherIcon (temperature: Double) -> UIImage {
          if temperature < 10 {
             return UIImage(systemName: "cloud.fill")!
         } else if temperature >= 10 && temperature < 20 {
             return UIImage(systemName: "cloud.sun.fill")!
         }
         return UIImage(systemName: "sunrise.fill")!
+    }
+    func obtainLastTimeRequestText(newTime: Int) -> String {
+        if newTime <= 0 {
+            return "Agora mesmo"
+        }
+        if newTime > 60 {
+            return "Há mais de uma hora atrás"
+        }
+        return "\(newTime) minuto\(newTime>1 ? "s": "") atrás"
+    }
+    func obtainTemperatureText(newValue: Int) -> String {
+        let unity = "˚C"
+        if newValue > 99 {
+            return "99\(unity)"
+        }
+        if newValue < -99 {
+            return "-99\(unity)"
+        }
+        return "\(newValue)\(unity)"
     }
 }
--- HelpMySkinTests/TodaysRoutineUnityTests/WeatherImageUnityTest.swift
+++ HelpMySkinTests/TodaysRoutineUnityTests/WeatherImageUnityTest.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import HelpMySkin
 
-class TodaysRoutineUnityTest: XCTestCase {
+class WeatherImageUnityTest: XCTestCase {
     var controller : TodaysRoutineViewController!
 
     override func setUpWithError() throws {
@@ -17,23 +17,23 @@ class TodaysRoutineUnityTest: XCTestCase {
 
     override func tearDownWithError() throws {}
     
-    func test_changeWeatherImage_shouldBeCloud() {
+    func test_obtainWeatherImage_shouldBe_Cloud() {
         //given
         let temperature = -25.0
         
         //when
-        let imageResult = controller.changeWeatherIcon(temperature: temperature)
+        let imageResult = controller.obtainWeatherIcon(temperature: temperature)
         
         //then
         XCTAssertEqual(imageResult, UIImage(systemName: "cloud.fill"))
     }
     
-    func test_changeWeatherImage_shouldBeSunrise() {
+    func test_obtainWeatherImage_shouldBe_Sunrise() {
         //given
         let temperature = 50.0
         
         //when
-        let imageResult = controller.changeWeatherIcon(temperature: temperature)
+        let imageResult = controller.obtainWeatherIcon(temperature: temperature)
         
         //then
         XCTAssertEqual(imageResult, UIImage(systemName: "sunrise.fill"))
