c45d54bc7a7f200b8bbf86b14f0df2fcab1a6a5f
Lais Godinho <lais2bg@gmail.com>
2022-06-21 09:58:59 -0300

    tableView adicionada a todaysRoutine

---
+++ HelpMySkin/Sources/Models/Notification.swift
@@ -0,0 +1,26 @@
+//
+//  Notification.swift
+//  HelpMySkin
+//
+//  Created by Lais Godinho on 21/06/22.
+//
+
+import Foundation
+
+struct Notification: Identifiable {
+  var id: UUID
+  var title: String
+  var description: String
+  init(id: UUID = UUID(), title: String, description: String) {
+    self.id = id
+    self.title = title
+    self.description = description
+  }
+}
+
+extension Notification {
+  static var sampleData = [
+    Notification(title: "Bom dia!", description: "Comece seus cuidados de hoje"),
+    Notification(title: "Boa noite!", description: "Não esqueça dos cuidados da noite!")
+  ]
+}
--- HelpMySkin/Sources/View/TodaysRoutine/TodaysRoutineViewController.swift
+++ HelpMySkin/Sources/View/TodaysRoutine/TodaysRoutineViewController.swift
@@ -12,9 +12,14 @@ class TodaysRoutineViewController: UIViewController {
     @IBOutlet weak var weatherCardImage: UIImageView!
     @IBOutlet weak var lastTimeRequestText: UILabel!
     @IBOutlet weak var temperatureText: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    public var notifications: [Notification] = Notification.sampleData
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         weatherCardImage.image = obtainWeatherIcon(temperature: 10)
         lastTimeRequestText.text = obtainLastTimeRequestText(newTime: 61)
         temperatureText.text = obtainTemperatureText(newValue: -30)
@@ -46,4 +51,15 @@ class TodaysRoutineViewController: UIViewController {
         }
         return "\(newValue)\(unity)"
     }
+}
+
+extension TodaysRoutineViewController: UITableViewDataSource, UITableViewDelegate {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return notifications.count
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = notifications[indexPath.row].title
+    return cell
+  }
 }
